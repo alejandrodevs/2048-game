2f126586d1c6f58d5bbc0d906ddc475b5ccdfcf3
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2014-07-17 23:51:14 -0500

    Using requestAnimationFrame to refresh game state

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,8 +1,15 @@
 (function(){
 
-  Game2048 = {}
-
   var ImageTypes = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192];
+
+  Game2048 = function() {
+    this.board = new Board();
+    this.board.initialize();
+
+    this.start = function() {
+      requestAnimationFrame(this.start.bind(this));
+    }
+  }
 
   Game2048.Image = function() {
     this.loaded = false,
@@ -15,11 +22,11 @@
 
   var Board = Game2048.Board = function() {
     this.initialize = function() {
+      this.canvas = document.createElement('canvas');
+      this.ctx = this.canvas.getContext('2d');
+      this.canvas.width = 500;
+      this.canvas.height = 500;
+      document.body.appendChild(this.canvas);
     }
-  }
-
-  Game2048.initialize = function() {
-    this.board = new Board();
-    this.board.initialize();
   }
 })();
